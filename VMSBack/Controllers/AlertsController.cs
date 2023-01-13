d56f067313ba372967bda33005e9afb9e7974d33
omar <omar.zabin@gmail.com>
2023-01-13 22:58:52 +0300

    Edit Alerts Controller

--- VMSBack/Controllers/AlertsController.cs
+++ VMSBack/Controllers/AlertsController.cs
@@ -15,34 +15,22 @@ namespace VMSBack.Controllers
         public AlertsController(IConfiguration configuration)
         {
             _config = configuration;
-            
+
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Alerts>>> GetAlerts()
+        public async Task<ActionResult<IEnumerable<Alerts>>> GetAlerts(string IMEI)
         {
             using var connVMS = new SqlConnection(_config.GetConnectionString("DefaultConnection3"));
             var registration = await connVMS.QueryAsync<Alerts>
-                (@" select TOP 100 * from [AllEvents] where VehicleID = 460 order by GPSTime desc");
+                (@"use TrackingNDB 
+                    select AE.AddressAr,AE.DeviceIMEI,AE.ExtendedProperties,AE.GPSTime,AE.Latitude,AE.Longitude,AE.Odometer,AE.Speed,AE.StreetSpeed,AE.VehicleIGN
+                    from AllEvents AE
+                    INNER JOIN VMSDB.dbo.Vehicles V ON V.DeviceIMEI = AE.DeviceIMEI 
+                    where V.DeviceIMEI = @IMEI", new { IMEI });
 
             return Ok(registration);
         }
     }
 
-    public class Alerts
-    {
-        public int VehicleID { get; set; }
-        public string TypeAr { get; set; }
-        public string SubTypeAr { get; set; }
-        public string ManufacturingYear { get; set; }
-        public int LocationID { get; set; }
-        public string DeviceIMEI { get; set; }
-        public DateTime GPSTime { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public int Speed { get; set; }
-        public bool VehicleIGN { get; set; }
-        public double Odometer { get; set; }
-        public string AddressAr { get; set; }
-    }
 }
---
+++ VMSBack/Src/Alerts.cs
@@ -0,0 +1,23 @@
+﻿namespace VMSBack.Src
+{
+
+    public class Alerts
+    {
+   
+        public string AddressAr { get; set; } =string.Empty;
+        public string DeviceIMEI { get; set; } = string.Empty;
+        public string ExtendedProperties { get; set; } = string.Empty;  
+        public DateTime GPSTime { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Odometer { get; set; }
+        public int Speed { get; set; }
+        public int StreetSpeed { get; set; }
+        public bool VehicleIGN { get; set; }
+
+
+    }
+ 
+
+
+}
