9fa91445820e92de570f0bec57fdb5e86605010d
omar <omar.zabin@gmail.com>
2023-01-19 06:11:29 +0300

    editing error

--- VMSBack/Src/RepairRecords.cs
+++ VMSBack/Src/RepairRecords.cs
@@ -4,7 +4,7 @@
     {
         public int RepId { get; set; }
         public int VehicleId { get; set; }
-        public string RepairDescription {get;set;} =string.Empty
+        public string RepairDescription { get; set; } = string.Empty;
         public string WorkShop { get; set; } = string.Empty;
         public string Cost { get; set; } = string.Empty;
         
